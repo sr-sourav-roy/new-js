52f315ad1a1c149da4508fc43102cb9b9f5180ff
Sourav Roy <sourav.cp.bd@gmail.com>
2025-08-10 03:44:32 +0600

    first commit

---
+++ Exx.py
@@ -0,0 +1,14 @@
+# Exception is a erro handaling so it is problam solving
+sourav = int(input("Give me a number: "))
+
+try: # try and axcept that is so erro handaling managment
+    print(20/sourav)
+except Exception as asha:
+    print(f"sorry there is an asha as {asha}")
+else:
+    print("There is run by good code")
+finally:
+    print("I will run no matter what!")
+
+print("sourav is very fucking man😁")
+
---
+++ index.js
@@ -0,0 +1,6 @@
+<html>
+  <title>Amar nam</title>
+  <body>
+    <p>amar nam sourab</p>
+  </body>
+</html>;
